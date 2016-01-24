eb0c57d70d3f1e626a670508c62e2da24cfa4141
slam24 <luismartinez242011@gmail.com>
2016-01-23 22:25:15 -0600

    feat: customize admin

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ["title","updated","timestamp"] # campos a mostrar
+	list_display_links = ["updated"] #campo link para editar
+	list_editable = ["title"]#campo editable
+	list_filter = ["updated","timestamp"]#filtros de fechas en la parte derecha
+
+	search_fields = ["title","content"]#buscador
+	class Meta:
+		model = Post
+
+admin.site.register(Post, PostModelAdmin)
