92a1edae3e4fa1455abd9d56975f6e9599af481a
slam24 <luismartinez242011@gmail.com>
2016-01-24 14:30:29 -0600

    feat: new post

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     #  /posts/
     url(r'^posts/$', views.post, name='posts'),
-     #  /posts/5/
+    #  /posts/5/
     url(r'^posts/(?P<id>[0-9]+)/$', views.postshow, name='postshow'),
+    #  /posts/new/
+    url(r'^posts/new/$', views.postcreate, name='postcreate'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,21 +2,32 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Post
+from .form import PostForm
 
-# Create your views here.
+
 def home(request):
 	return render(request,"index.html",{})
 
 def post(request):
-	queryset = Post.objects.all()	
+	queryset = Post.objects.all()
 	context = {
 		'posts': queryset,
 	}
 	return render(request,"post/index.html",context)
 
 def postshow(request, id):
-	queryset = Post.objects.get(id=id)	
+	queryset = Post.objects.get(id=id)
 	context = {
 		'post': queryset,
 	}  
 	return render(request,"post/show.html",context)
+
+def postcreate(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+	context = {
+		"form": form,
+	}
+	return render(request,"post/new.html",context)
