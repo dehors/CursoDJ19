f0bbae3fd4eb606ae7afae1aeca9e072354cfa3a
slam24 <luismartinez242011@gmail.com>
2016-02-06 17:09:17 -0600

    feat: options imagefile

--- posts/models.py
+++ posts/models.py
@@ -3,10 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
+def upload_location(instance, filename):
+	filebase, extension = filename.split(".")
+	return "%s/%s.%s" %("post",instance.id, extension )
+	
 class Post(models.Model):
 	title = models.CharField(max_length=120)
-	image = models.FileField(null=True, blank=True)
+	image = models.ImageField(upload_to=upload_location, null=True, blank=True, width_field="width_field", height_field="height_field")
+	height_field = models.IntegerField(default=0)
+	width_field = models.IntegerField(default=0)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
